96078e0ad9488f85ec35e8ff9d3893a7948f8298
JessicaVF <villarjessica@hotmail.com>
2021-04-19 20:11:34 +0200

    Description of pokemon (only 'babies' and 'middle kids')

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//--> first we get the data about the first generation (151 pokemons and other info)
+// Src= The XHR part is based in: https://attacomsian.com/blog/http-requests-xhr
+
 // create an XHR object
 const xhr = new XMLHttpRequest();
 
@@ -21,11 +24,13 @@ xhr.open("GET", "https://pokeapi.co/api/v2/generation/1/");
 // send request
 xhr.send();
 
+// --> Once we have the info about the generation we focus in get just the pokemons (putting aside locations, for example)
 function pokeProcess(pokeJson) {
   let pokeList = pokeJson.pokemon_species;
 
-  for (let i = 0; i < pokeList.length; i++) {
+  for (let i = 0; i < 7; i++) {
     let poke = pokeList[i];
+
     let pokeSection = $("<section>");
 
     let id = getPokeId(poke.url);
@@ -35,11 +40,10 @@ function pokeProcess(pokeJson) {
     let pokeTitle = $("<h2>");
     pokeTitle.text(poke.name);
     pokeSection.append(pokeTitle);
-    pokeSection.append($("<p>"));
 
     $("main").append(pokeSection);
 
-    getChain(poke.url, poke.name);
+    getChain(poke.url, poke.name, id);
   }
 }
 function getPokeId(pokeUrl) {
@@ -48,20 +52,20 @@ function getPokeId(pokeUrl) {
   return id;
 }
 
-function getChain(pokeUrl, pokeName) {
+function getChain(pokeUrl, pokeName, id) {
   fetch(pokeUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      poke_Evolution(data.evolution_chain.url, pokeName);
+      poke_Evolution(data.evolution_chain.url, pokeName, id);
     })
     .catch(function (error) {
       console.log("error 2");
     });
 }
 
-function poke_Evolution(evolution_chain, pokeName) {
+function poke_Evolution(evolution_chain, pokeName, id) {
   fetch(evolution_chain)
     .then(function (response) {
       return response.json();
@@ -71,24 +75,26 @@ function poke_Evolution(evolution_chain, pokeName) {
       let pokeMedium = data.chain.evolves_to[0].species.name;
       //   let pokeLarge = data.chain.evolves_to[0].evolves_to[0].species.name;
 
-      console.log(pokeSmall);
-      console.log(`into ${pokeMedium}`);
-      if (pokeName == pokeSmall) {
-        $("p").text(
-          `This pokemon is the baby of the house, it evolves into ${pokeMedium}`
-        );
-        console.log("eureka");
-      } else {
-        console.log("pas eureka");
-      }
-
       //   console.log("hello: " + data.chain.species.name);
       //   console.log("Good day: " + data.chain.evolves_to[0].species.name);
       //   console.log(
       //     "Greetings: " + data.chain.evolves_to[0].evolves_to[0].species.name
       //   );
+      if (pokeName == pokeSmall) {
+        let pokeSection = $(`[data-id='${id}']`);
+        let text = `${pokeName} is the baby of the house, it evolves into ${pokeMedium}`;
+        pokeSection.append(text);
+      } else if (pokeName == pokeMedium) {
+        let pokeSection = $(`[data-id='${id}']`);
+        let text = `${pokeName} is the middle kid of the house, is the evolution of ${pokeSmall} and it evolves into ${pokeLarge}`;
+        pokeSection.append(text);
+      } else if (pokeName == pokeLarge) {
+        let pokeSection = $(`[data-id='${id}']`);
+        let text = `${pokeName} is the older kid of the house, is the evolution of ${pokeMedium}`;
+        pokeSection.append(text);
+      }
     })
     .catch(function (error) {
-      console.log("error 3");
+      console.log("error 3 " + pokeName);
     });
 }
